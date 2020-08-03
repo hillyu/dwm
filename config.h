c98eb9a6928d2bf9a4e87016db528f7c1cf77785
Hill <hillyu@live.com>
2020-08-03 20:57:34 +0800

    adjust layourt key

--- config.h
+++ config.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1. ", "2.", "3.", "4. ", "5.", "6.", "7.", "8.", "9." };
+static const char *tags[] = { "1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9." };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "rofi", "-show", "combi", "-monitor", "-4"};
+static const char *dmenucmd[] = { "rofi", "-show", "combi", "-m", "-4"};
 static const char *termcmd[]  = { "st", NULL};
 static const char *snapshot_full[]  = { "flameshot", "full", "-c", "-p", "/home/hill/Pictures/screenshot/" };
 static const char *snapshot[]  = { "flameshot", "gui" };
@@ -112,9 +112,9 @@ static Key keys[] = {
 	{ ControlMask,                  XK_Tab,    view,           {0} },
 	{ ControlMask,                  XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_e,      cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_f,      togglefullscr,     {0} },
