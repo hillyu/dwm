d9b661ca48528df45ac79802ec8f1839e73f25de
Hill <hillyu@live.com>
2020-08-15 13:11:35 +0800

    losefocus

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -33,8 +33,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class          instance       title       tags mask     iscentered isfloating   monitor */
-	{ "Gimp",         NULL,          NULL,       1 << 6,            1,         1,           -1 },
+	{ "Gimp",         NULL,          NULL,       1 << 6,       1,         1,           -1 },
 	{ "imv",          NULL,          NULL,       0,            1,         1,           -1 },
+	{ "Nitrogen",     NULL,          NULL,       0,            1,         1,           -1 },
 	{ "mpv",          NULL,          NULL,       0,            1,         1,           -1 },
 	{ "feh",          NULL,          NULL,       0,            1,         1,           -1 },
 	{ "tabbed",       NULL,          NULL,       0,            0,         1,           -1 },
@@ -83,7 +84,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-show", "combi", "-m", "-4"};
-static const char *termcmd[]  = { "st", "-c", "prompt", "-g", "159x46", "tmux_run.sh"};
+static const char *termcmd[]  = { "st", "-c", "prompt", "-g", "158x46", "tmux_run.sh"};
 static const char *snapshot_full[]  = { "flameshot", "full", "-c", "-p", "/home/hill/Pictures/screenshot/" };
 static const char *snapshot[]  = { "flameshot", "gui" };
 static const char *brightness_minus[]  = { "brightness", "1%-" };
